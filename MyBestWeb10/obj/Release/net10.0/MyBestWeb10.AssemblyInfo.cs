7c33a077aa2932b5209a5eb1837fa839d3e0f094
Baskaynak <subaskaynak07@gmail.com>
2026-06-03 14:50:49 +0300

    DevOps: Statik HTML sayfaları başarıyla kök dizine eklendi

--- MyBestWeb10/obj/Release/net10.0/MyBestWeb10.AssemblyInfo.cs
+++ MyBestWeb10/obj/Release/net10.0/MyBestWeb10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBestWeb10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3affc2e462b3deed25b1c690b955d427bc932f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5148bee3978b58a052be2fe78654108dc6ad59")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBestWeb10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBestWeb10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
